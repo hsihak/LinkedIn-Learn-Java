b1dcd2c9460f17b5c7eaf5ab4d3d1dffa587dae8
Hangsihak Sin <Hsin9104@conestogac.on.ca>
2024-01-07 23:13:50 -0500

    add: first hello world java program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// File name and class must be identical to each other, otherwise it will cause errors
+public class Main {
+    // Entry point to our program - boilerplate
+    public static void main(String[] args) {
+        System.out.println("Hello World! It's me, Shaq");
+    }
+}
